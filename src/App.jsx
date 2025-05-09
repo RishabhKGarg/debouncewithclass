cc14d309427c86b26466adc1971510083dffec0c
RishabhKGarg <gargrishabhofficial7@gmail.com>
2025-05-09 11:01:31 +0530

    cha ge in main.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,52 @@
 import React, { useState } from 'react'
 import { Component } from 'react'
 class Xyz extends Component {
-  constructor(){
+  constructor() {
     super()
-   
-    this.state={allusers:["rishabh","aparna","shilpi","ritu","kamlesh"],filteredusers:[],userinput:"",debounceduserinput:""}
+
+    this.state = { allusers: ["rishabh", "aparna", "shilpi", "ritu", "kamlesh"], filteredusers: [], userinput: "", debounceduserinput: "" }
+    this.abc = null;
   }
-   pqr=(e)=>{
-    this.setState({userinput:e.target.value})
+  pqr = (e) => {
+    this.setState({ userinput: e.target.value })
   }
-  
-  componentDidUpdate(prevprops,prevstate){
-    if(prevstate.userinput!==this.state.userinput){
-      setTimeout(()=>{
-        this.setState({debounceduserinput:userinput})
-      },[2000])
+
+  componentDidUpdate(prevprops, prevstate) {
+    if (prevstate.userinput !== this.state.userinput&&this.state.userinput!=="") {
+      if (this.abc) {
+        clearTimeout(this.abc)
+      }
+      this.abc = setTimeout(() => {
+        this.setState({ debounceduserinput: this.state.userinput })
+      },500)
     }
+    if (prevstate.debounceduserinput !== this.state.debounceduserinput) {
+      if(this.state.debounceduserinput!==""){
+
+        var alluser = this.state.allusers.filter((user) => {
+          return user.includes(this.state.debounceduserinput)
+        })
+        this.setState({ filteredusers: alluser })
+      }
+      else{
+        this.setState({filteredusers:[]})
+      }
+
+      // if (JSON.stringify(alluser) !== JSON.stringify(this.state.filteredusers)) {
+      //   this.setState({ filteredusers: alluser });
+      // }
+
+    }
+     
   }
-  componentDidUpdate(prevprops,prevstate){
-    if(prevstate.debounceduserinput!==this.state.debounceduserinput){
-    var alluser=  this.state.allusers.filter((user)=>{
-      return user.includes(this.state.debounceduserinput)
-      })
-    } 
-    this.setState({filteredusers:alluser})
-  }
-  
+
   render() {
     return (
       <>
-      <input type="text" onChange={this.pqr} />
+        <input type="text" onChange={this.pqr} />
         {
-        this.state.filteredusers.map((user,index)=>{
-            return(
+          this.state.filteredusers.map((user, index) => {
+            return (
               <div key={index}>{user}</div>
             )
           })
